b8961de38b1f80099e1583befe4a6f13f69ed944
ifTNT <happyjohn369@gmail.com>
2020-06-27 16:23:48 +0800

    Completed exhibition information

---
+++ routes/EPIS/exhibitionInformation.js
@@ -0,0 +1,83 @@
+const db = require("../../db");
+
+function addExhibition(year) {
+  return new Promise((resolve, reject) => {
+    if (year === undefined) reject({ message: "Invalid argument" });
+    db.Exhibition.create(
+      {
+        year: year, //民國年度，integer
+        date: `${year + 1911}-9-31`,
+        location: "國立高雄大學圖書資訊館二樓",
+        mapB64: "",
+        poster: "",
+        freezed: false,
+      },
+      (err, data) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(data);
+        }
+      }
+    );
+  });
+}
+
+// Return all of the exhibitions
+function getExhibitions() {
+  return new Promise((resolve, reject) => {
+    db.Exhibition.find({}, (err, data) => {
+      if (err) reject(err);
+      resolve(data);
+    });
+  });
+}
+
+// Return spicificed year exhibition
+function getExhibition(year = 0) {
+  return new Promise((resolve, reject) => {
+    db.Exhibition.findOne({ year: year }, (err, data) => {
+      if (err) reject(err);
+      resolve(data);
+    });
+  });
+}
+
+// Update specified year exhibition
+function setExhibition(year = 0, newContent = {}) {
+  return new Promise((resolve, reject) => {
+    db.Exhibition.findOneAndUpdate(
+      { year: year },
+      newContent,
+      { omitUndefined: true, new: true },
+      function (err, data) {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(data);
+        }
+      }
+    );
+  });
+}
+
+// Freeze the specified year exhibition
+function freezeExhibition(year = 0) {
+  return new Promise((resolve, reject) => {
+    setExhibition(year, { freezed: true })
+      .then((d) => {
+        resolve(d);
+      })
+      .catch((e) => {
+        reject(e);
+      });
+  });
+}
+
+module.exports = {
+  addExhibition,
+  getExhibitions,
+  getExhibition,
+  setExhibition,
+  freezeExhibition,
+};
--- routes/EPIS/index.js
+++ routes/EPIS/index.js
@@ -1,9 +1,60 @@
-var express = require('express');
+var express = require("express");
+var exhibitionInformation = require("./exhibitionInformation");
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+// Add an exhibition
+router.get("/exhib/add/:year", function (req, res, next) {
+  if(req.params.year===undefined){
+    next({message: 'Invalid argument'});
+  }else{
+  let year = parseInt(req.params.year);
+  exhibitionInformation
+    .addExhibition(year)
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => next(err));
+  }
+});
+
+// Get all of the exhibitions
+router.get("/exhibs", function (req, res, next) {
+  exhibitionInformation
+    .getExhibitions()
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => next(err));
+});
+
+// Get an exhibition
+router.get("/exhib/:year", function (req, res, next) {
+  if(req.params.year===undefined){
+    next({message: 'Invalid argument'});
+  }else{
+  let year = parseInt(req.params.year);
+  exhibitionInformation
+    .getExhibition(year)
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => next(err));
+  }
+});
+
+// Freeze an exhibition
+router.get("/exhib/:year/freeze", function (req, res, next) {
+  if(req.params.year===undefined){
+    next({message: 'Invalid argument'});
+  }else{
+  let year = parseInt(req.params.year);
+  exhibitionInformation
+    .freezeExhibition(year)
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => next(err));
+  }
 });
 
 module.exports = router;
